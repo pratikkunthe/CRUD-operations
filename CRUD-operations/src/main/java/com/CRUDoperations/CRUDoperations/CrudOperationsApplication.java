d6b09385d7f81be56df54aafc52d494a200dd88a
pratikkunthe <100013276+pratikkunthe@users.noreply.github.com>
2023-09-23 23:11:01 +0530

    first commit

---
+++ CRUD-operations/src/main/java/com/CRUDoperations/CRUDoperations/CrudOperationsApplication.java
@@ -0,0 +1,15 @@
+package com.CRUDoperations.CRUDoperations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudOperationsApplication.class, args);
+		System.out.println("Welcome to CRUD");
+	}
+
+
+}
